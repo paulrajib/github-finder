105f1bfe53258a78183ce4b9b661d53fa236cc90
paulrajib <rajib.ece09@gmail.com>
2020-01-13 15:41:55 +0600

    class based but state less but with props with static data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
@@ -11,7 +11,9 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar title='Github Finder' />
-        <UserItem />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
