077290bf0753d255d051e313a28f73e397f35272
paulrajib <rajib.ece09@gmail.com>
2020-01-13 20:30:05 +0600

    No class but function based but setStateinto class with propTypes, Spinner.gif loading screen, with grid, with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    // axios
+    //   .get("https://api.github.com/users")
+    //   .then(res => console.log(res.data));
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    // console.log(res.data);
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     // const name = "Rajib Paul";
     // const loading = false;
@@ -12,7 +29,7 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Finder' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import PropTypes from "prop-types";
 
 // const element = <FontAwesomeIcon icon={faGithub} />;
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder"
-  };
-  render() {
-    return (
-      <div className='navbar bg-primary'>
-        {/* <h1>{element} NavBar</h1> */}
-        <h1>
-          <FontAwesomeIcon icon={faGithub} /> {this.props.title}
-        </h1>
-      </div>
-    );
-  }
-}
+//const Navbar = props => {
+const Navbar = ({ title }) => {
+  //render() {
+  return (
+    <div className='navbar bg-primary'>
+      {/* <h1>{element} NavBar</h1> */}
+      <h1>
+        <FontAwesomeIcon icon={faGithub} /> {title}
+      </h1>
+    </div>
+  );
+  //}
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder"
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Navbar;
